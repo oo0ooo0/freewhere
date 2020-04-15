7be934c59955ebb7f6fe44aabe96d4c28fd6b6ab
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-16 03:33:18 +0900

    add applyMiddleware
    reduxThunk & REDUX_DEVTOOL

--- src/services/store.jsx
+++ src/services/store.jsx
@@ -1,8 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from './reducers';
 
 import reduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(reduxThunk));
+
+const store = createStore(rootReducer, /* preload */ enhancer);
 
 export default store;
