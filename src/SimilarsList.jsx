06a3b0626eead9c6227c54418de10923121a7b27
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-16 01:19:08 +0900

    add List common style to GlobalStyled

--- src/SimilarsList.jsx
+++ src/SimilarsList.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import SimillarItem from './components/SimillarItem';
 import Masonry from 'react-masonry-css';
 import { TitleContainer } from './components/Containers';
+import { useDispatch } from 'react-redux';
+import { getSimilars } from './services/simillars/actions';
 
 const StyledSimilarsList = styled.main`
-  width: 49.8%;
-
-  .my-masonry-grid {
+  h3 {
+    background: #fafafa;
     display: flex;
-    width: auto;
-  }
-  .my-masonry-grid_column {
-    background-clip: padding-box;
-  }
-
-  .my-masonry-grid_column > div {
-    margin-bottom: 8px;
+    align-items: center;
+    margin-top: 3px;
+    padding-left: 25px;
+    height: 36px;
   }
 `;
 
 function SimilarsList() {
-  // const feeds = useSelector((state) => state.feeds);
-  return (
-    <StyledSimilarsList>
-      <TitleContainer align={'center'}>문항 교체/추가</TitleContainer>
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getSimilars());
+    console.log('call?');
+  }, [dispatch]);
+  return (
+    <StyledSimilarsList className='list'>
+      <TitleContainer align={'center'}>문항 교체/추가</TitleContainer>
+      <h3>문제유형을 잘 넣기</h3>
       <Masonry
         breakpointCols={1}
         className='my-masonry-grid'
@@ -37,7 +39,7 @@ function SimilarsList() {
         {/* {Object.keys(feeds)
           .reverse()
           .map((key) => {
-            return <Item key={key} {...feeds[key]} />;
+            return <SimillarItem key={key} {...simillar[key]} />;
           })} */}
       </Masonry>
     </StyledSimilarsList>
