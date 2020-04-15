427b075afee90f0f29491fd7947227eeff4067a7
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-15 18:26:36 +0900

    add Container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
+import { LayoutContainer } from './components/Containers';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
+
+      <LayoutContainer></LayoutContainer>
     </div>
   );
 }
---
+++ src/components/Containers.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.div`
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  background-color: #f2f2f2;
+`;
+
+export const TitleContainer = styled.h2`
+  width: 100%;
+  padding: 13px 24px;
+  align-items: center;
+  position: relative;
+  text-align: ${(props) => props.align || 'left'};
+  margin-bottom: 3px;
+  background-color: #fff;
+`;
