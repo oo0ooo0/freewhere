434cea7984de5b15e5c3ee4b30cea7d31aec5920
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-16 11:17:25 +0900

    add problemUnit wrap

--- src/SimilarsList.jsx
+++ src/SimilarsList.jsx
@@ -9,6 +9,12 @@ import { getSimilars } from './services/similars/actions';
 import { useSelector } from 'react-redux';
 
 const StyledSimilarsList = styled.main`
+  .SimilarsListTitleCont {
+    border-bottom: 3px solid #f5f5f5;
+    z-index: 10;
+    position: absolute;
+    width: 49.8%;
+  }
   h3 {
     background: #fafafa;
     display: flex;
@@ -16,6 +22,9 @@ const StyledSimilarsList = styled.main`
     margin-top: 3px;
     padding-left: 25px;
     height: 36px;
+  }
+  .my-masonry-grid {
+    margin-top: 10px;
   }
 `;
 
@@ -30,24 +39,27 @@ function SimilarsList() {
 
   return (
     <StyledSimilarsList className='list'>
-      <TitleContainer align={'center'}>문항 교체/추가</TitleContainer>
+      <TitleContainer className='SimilarsListTitleCont' align={'center'}>
+        문항 교체/추가
+      </TitleContainer>
+
       {problems.selectedId ? (
-        <Masonry
-          breakpointCols={1}
-          className='my-masonry-grid'
-          columnClassName='my-masonry-grid_column'
-        >
-          {similars.items.map((similar) => {
-            return <SimilarItem key={similar.id} {...similar} />;
-          })}
-        </Masonry>
+        <div>
+          <h3>문제유형을 잘 넣기</h3>
+
+          <Masonry
+            breakpointCols={1}
+            className='my-masonry-grid'
+            columnClassName='my-masonry-grid_column'
+          >
+            {similars.items.map((similar) => {
+              return <SimilarItem key={similar.id} {...similar} />;
+            })}
+          </Masonry>
+        </div>
       ) : (
         <Clickmessage />
       )}
-
-      {/* 
-      <h3>문제유형을 잘 넣기</h3>
-       */}
     </StyledSimilarsList>
   );
 }
